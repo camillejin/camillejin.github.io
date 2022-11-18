5aab4f995c8790a967363232d31dec3f3caa2ae8
camillejin <camillejin@yonsei.ac.kr>
2022-11-18 10:29:14 +0900

    gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 // add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
 const shapeItems = document.querySelectorAll('.nav-shape');
+const imageHolder = document.getElementById('displayImage');
 
 const path = window.location.pathname;
 const pathList = path.split(/[./]/);
@@ -17,6 +18,12 @@ shapeItems.forEach(function(thisItem) {
         }
     }
 });
+
+
+function hover(element) {
+    imageHolder.setAttribute('src', element.getAttribute('src'));
+}
+  
 
 /*
 

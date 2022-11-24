33855519f221976a5d2761e1a6832f39e1d531e3
camillejin <camillejin@yonsei.ac.kr>
2022-11-25 04:14:30 +0900

    nov 25

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,14 +44,24 @@ function changeText(element, text) {
 
 
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 
 
 
-  
+
+
 
 /*
-
 window.onscroll = function() {
     var top = window.scrollY;
     if(top >=100) {

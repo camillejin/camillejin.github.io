c78bb770167b89adf417cd742407f4aff281345f
camillejin <camillejin@yonsei.ac.kr>
2022-11-29 20:56:29 +0900

    font delete

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,5 +57,10 @@ window.onscroll = function() {
 }
 
 
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
 
 
